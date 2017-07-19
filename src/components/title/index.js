a99053bfbf9c0d0609a4fa91917ca8bc0e8d86fe
mvfsillva <mvfsillva@gmail.com>
2017-07-19 14:25:20 -0300

    Refactor boilerplate

--- src/components/title/index.js
+++ src/components/title/index.js
@@ -1,9 +1,5 @@
-'use strict'
-
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import './title.css'
 
 const Title = ({ children }) => (
   <h1>{children}</h1>
--- test/title.test.js
+++ test/title.test.js
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Title from './index'
+import Title from '../src/components/title/index'
 
 it('Should Title match to snapshot', () => {
   const tree = renderer.create(<Title>Título</Title>).toJSON()
